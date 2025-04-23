64ed27e1ba366cbd6a58d10fc2542264370f07be
alfoCaiazza <acaiazza2000@gmail.com>
2025-04-23 11:01:26 +0200

    Added another condition on filtering reviews

--- src/scripts/handlig_user.py
+++ src/scripts/handlig_user.py
@@ -1,69 +1,53 @@
 import pandas as pd
 from pathlib import Path
-import sys
 import logging
 import numpy as np
 
 PROJECT_ROOT = Path(__file__).parent.parent
-INPUT_PATH = PROJECT_ROOT / "data" / "raw" / "reviews.csv"
+INPUT_PATH = PROJECT_ROOT / "data" / "processed" / "cleaned_reviews.csv"
 
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def validate_paths(path):
+def validate_paths(path: Path):
     if not path.exists():
         raise FileNotFoundError(f"Data or file not founded: {path}")
     if not path.parent.exists():
         raise FileNotFoundError(f"Directory not founded: {path.parent}")
 
-def load_dataset(path:str) -> pd.DataFrame:
+def load_dataset(path: Path) -> pd.DataFrame:
     validate_paths(path)
-        
-    logger.info("Loading dataset...")
+    
+    logger.info(f"Loading dataset from {path}")
     try:
         df = pd.read_csv(path)
         if df.empty:
-            raise ValueError("Loaded empy DataFrame")
+            raise ValueError("Loaded empty DataFrame")
     except Exception as e:
-        logger.error(f"ERROR in loading dataset: {str(e)}")
+        logger.error(f"Error loading dataset: {e}")
         raise
-
     return df
 
-# def load_embeddings(path:str) -> np.ndarray:
-#     validate_paths(path)
-        
-#     logger.info("Loading recipe embeddings...")
-#     try:
-#         embeddings = np.load(path)
-#     except Exception as e:
-#         logger.error(f"ERROR in loading embeddings: {str(e)}")
-#         raise
-
-#     return embeddings
-
-def get_user_history(df: pd.DataFrame, user_id: int) -> np.ndarray:
+def get_user_history(df: pd.DataFrame, user_id: int, label: int) -> np.ndarray:
     """
-    Returns the embedding of the recipe with the given RecipeId.
-    Assumes embeddings[i] corrisponde a df.iloc[i].
+    Retrives the review ids and the recipes ids of the reviews made by a specified user
     """
-    logger.info(f"Searching recipe  for user id : {user_id}")
-    try:
-        reviews_history = df['RecipeId'].loc[df['AuthorId'] == user_id]
-        
-        if reviews_history.empty:
-            raise ValueError(f"No reviews history found for user = {user_id}")
-    except Exception as e:
-        logger.error(f"ERROR in finding recipe embedding: {str(e)}")
-        raise
+    logger.info(f"Retrieving recipe history for user ID: {user_id}")
+    reviews_history = df.loc[(df['AuthorId'] == user_id) & (df['Label'] == label), 'RecipeId'].values
+    
+    if len(reviews_history) == 0:
+        error_msg = f"No review history found for user ID {user_id}"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
 
     return reviews_history
 
-
 if __name__ == "__main__":
     df = load_dataset(INPUT_PATH)
-    # embeddings = load_embeddings(EMBEDDINGS_PATH)
-    output = get_user_history(df, 2046)
-    print(f"Total reviews retrived: {len(output)}\n")
-    print(output)
+    user_id = 2046
+    label = 0
+    recipes_reviewed = get_user_history(df, user_id, label)
+
+    logger.info(f"Total reviews retrieved for user {user_id}: {len(recipes_reviewed)}")
+    print(f"Recipe IDs:\n{recipes_reviewed}")
